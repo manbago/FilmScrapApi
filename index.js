76981cbd821f1fe231aec27fe1cc3e7d50d520f1
manbago <manbago@gmail.com>
2021-12-01 18:42:51 +0100

    FilmApi and Scrap

--- index.js
+++ index.js
@@ -1,17 +1,122 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-
+const express = require("express");
+const bodyParser = require("body-parser");
 const apiRouter = require("./routes/api");
+
+const puppeteer = require("puppeteer");
+
 const app = express();
 
-require ('./db');
+require("./db");
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.use("/api", apiRouter);
 
+// Launching the Puppeteer controlled headless browser and navigate to the Digimon website
+const total = 2;
+let hrefsTotal = [];
+var results = [];
 
+for (let index = 1; index < total; index++) {
+  (async function main() {
+    try {
+      const browser = await puppeteer.launch();
+      const [page] = await browser.pages();
+
+      await page.goto("https://todotorrents.net/peliculas/hd/page/" + index);
+
+      // way 1
+      const hrefs1 = await page.evaluate(() =>
+        Array.from(document.querySelectorAll(".text-center a[href]"), (a) =>
+          a.getAttribute("href")
+        )
+      );
+
+      //console.log(hrefs1);
+      hrefsTotal = hrefsTotal.concat(hrefs1);
+      console.log(hrefsTotal);
+
+      for (let index2 = 0; index2 < hrefs1.length; index2++) {
+        const [page2] = await browser.pages();
+        await page2.goto("https://todotorrents.net" + hrefs1[index2]);
+        //page2.goto(hrefs1[index2]);
+        await page2.waitForTimeout(500);
+        // call and wait extractedEvaluateCall and concatenate results every iteration.
+        // You can use results.push, but will get collection of collections at the end of iteration
+        results = results.concat(await extractedEvaluateCall(page2));
+        //console.log("tpta"+results);
+      }
+      console.log("RESULTADOS" + results);
+
+      await browser.close();
+    } catch (err) {
+      console.error(err);
+    }
+
+    var jsonData = JSON.stringify(results);
+    var fs = require("fs");
+    fs.writeFile("tester5.json", jsonData, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+
+    var jsonData = JSON.stringify(hrefsTotal);
+    var fs = require("fs");
+    fs.writeFile("tester6.json", jsonData, function (err) {
+      if (err) {
+        console.log(err);
+      }
+    });
+  })();
+
+  var jsonData = JSON.stringify(hrefsTotal);
+  var fs = require("fs");
+  fs.writeFile("tester.json", jsonData, function (err) {
+    if (err) {
+      console.log(err);
+    }
+  });
+} //fin for
+
+
+async function extractedEvaluateCall(page) {
+  // just extracted same exact logic in separate function
+  // this function should use async keyword in order to work and take page as argument
+
+  return page.evaluate(() => {
+    let title = document.querySelector(".card-body h2")?.innerText || "No title";
+    let description = document.querySelector(".text-justify").innerText;
+    let picture = document.querySelectorAll(".card-body > img");
+    let imagen = picture[0].src;
+    let releaseYear = document.querySelector(".d-inline-block p a").innerText;
+    let playersFilm = document.querySelector(".mb-0").innerText;
+    let format = document.querySelector(".text-center .d-inline-block p").innerText;
+    let size = document.querySelector(".d-inline-block .mb-0").innerText;
+    let torrent = Array.from(
+      document.querySelectorAll(".text-center a[href]"),
+      (a) => a.getAttribute("href")
+    ).toString();
+    let urlWeb = document.location.href;
+
+    return {
+      title,
+      description,
+      imagen,
+      releaseYear,
+      playersFilm,
+      format,
+      size,
+      torrent,
+      urlWeb,
+    };
+
+  });
+}
+
+
+// Making Express listen on port 7000
 app.listen(3000, () => {
-  console.log('Listening on port 3000');
+  console.log("Listening on port 3000");
 });
